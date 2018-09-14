06f1eabd1fd42ca2a78f54ea83c77b5b5b491ae2
perro-negro <gi.vega@itsur.edu.mx>
2018-09-14 10:02:34 -0500

    Se creo el recyvclerview y eL ADAPTADOR

--- app/src/main/java/net/ivanvega/fragmentosenandroid/HeadlinesFragment.java
+++ app/src/main/java/net/ivanvega/fragmentosenandroid/HeadlinesFragment.java
@@ -4,14 +4,29 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class HeadlinesFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] colores = {"Verde", "Blanco", "Rojo", "Axul"};
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        View layout = inflater.inflate(R.layout.headlines_layout, container, false);
+
+        mRecyclerView =  layout.findViewById(R.id.my_recycler_view);
+        mAdapter = new MyAdapter(colores);
+
+
+
+        return  layout;
+
     }
 }
